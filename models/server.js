26353b3ba0416fcc4a7aeb10a45f81ff31922053
LuisLunaV <luis.a.luna.v@outlook.com>
2022-08-16 23:24:12 -0500

    Se actualiza el puerto

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor(){
     this.app = express();
-    this.port = process.env.port;
+    this.port = process.env.PORT;
     this.usuariosRoutePath = '/api/usuarios';
 
     //Conectar a base de datos
